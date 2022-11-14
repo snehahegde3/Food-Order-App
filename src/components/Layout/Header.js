64649b91721cac59f74a1efa26135af678fa0b8b
Sneha Hegde <snehahegde33@gmail.com>
2022-11-14 17:51:15 +0530

    some components added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,24 @@
+//importing images
+
+//the extension matters since it is not a js file
+import mealsImg from '../../assets/meals.jpg';
+
+import classes from './Header.module.css';
+
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div>
+        <img className={classes['main-image']} src={mealsImg} alt='food' />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+// import classes from './Meals.module.css';
+
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
